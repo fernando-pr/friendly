d6f78f1e73699e3f9652c992aaf883c06e470306
Fernando Pacheco <fernando321987@gmail.com>
2017-04-10 14:50:05 +0200

    modificar y borrar solo usuario propietario

--- views/usuarios/view.php
+++ views/usuarios/view.php
@@ -6,23 +6,27 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Usuario */
 
-$this->title = $model->id;
+$this->title = 'Perfil ' . $model->nombre;
 $this->params['breadcrumbs'][] = (Yii::$app->user->esAdmin) ? ['label' => 'Usuarios', 'url' => ['index']] : 'Usuarios';
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $model->nombre;
 ?>
 <div class="usuario-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+
+    <h1><?= Html::encode($model->nombre) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
+        <?php if ($model->id == Yii::$app->user->id) {?>
+        <?= Html::a('Modificar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Borrar', ['delete', 'id' => $model->id], [
+           'class' => 'btn btn-danger',
+           'data' => [
+               'confirm' => '¿Seguro que desear borrar tu usuario?',
+               'method' => 'post',
+           ],
+      ]) ?>
+         <?php } ?>
+
     </p>
 
    <img src="<?= $model->imageUrl ?>" />
@@ -31,16 +35,13 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            //'id',
+
             'nombre',
-            //'password',
             'email:email',
             'poblacion',
             'provincia',
-            
-            //'token',
-            //'activacion',
-            //'created_at',
+
+
         ],
     ]) ?>
 
