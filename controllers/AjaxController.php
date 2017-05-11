2d1486bf4f873c0fc0af44c36f1483e05dcd6f0e
Fernando Pacheco <fernando321987@gmail.com>
2017-05-11 13:37:29 +0200

    al buscar tampoco muestra los usuarios que no estan activados

--- controllers/AjaxController.php
+++ controllers/AjaxController.php
@@ -31,12 +31,13 @@ class AjaxController extends \yii\web\Controller
     public function actionBuscar($q, $cond)
     {
         if ($cond == 'todos') {
-
             $model = Usuario::find()->orWhere(['ilike', 'nombre', $q])
                                       ->orWhere(['ilike', 'poblacion', $q])
-                                      ->orWhere(['ilike', 'provincia', $q])->all();
+                                      ->orWhere(['ilike', 'provincia', $q])
+                                      ->andWhere('activacion is null')
+                                      ->all();
         } else {
-            $model = Usuario::find()->where(['ilike', $cond, $q])->all();
+            $model = Usuario::find()->where(['ilike', $cond, $q])->andWhere('activacion is null')->all();
         }
 
         return $this->renderAjax('/site/_usuarios', [
